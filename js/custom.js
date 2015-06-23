6dac6634cffb4893a912aa90a06d001897a8fc00
taystu <s.taylor26@hotmail.com>
2015-06-23 11:17:43 -0400

    First commit

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
+ * Author:HtmlCoder
+ * Author URI:http://www.htmlcoder.me
+ * Version:1.0.0
+ * Created:November 2014
+ * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
+ * File Description: Place here your custom scripts
+ */
+
+
+$(document).ready(function() {
+$('.carousel').bcSwipe({ threshold: 40 });
+
+	$(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+
+
+
+});
+
