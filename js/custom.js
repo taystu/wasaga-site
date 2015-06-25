0a596b98c4b431a7b9f88747870688cf408aa050
taystu <s.taylor26@hotmail.com>
2015-06-25 11:21:20 -0400

    First commit

--- js/custom.js
+++ js/custom.js
@@ -21,39 +21,4 @@ $(document).ready(function() {
 		padding: 0
 	});
 
-	$('.slick-slide').slick({
-  		slidesToShow: 1,
-  		infinite: true,
-  		arrows: false,
-
-	});
-
 });
-
-
-var pswpElement = document.querySelectorAll('.pswp')[0];
-
-// build items array
-var items = [
-    {
-        src: 'images/gallery1.jpg',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'images/gallery2.jpg',
-        w: 1200,
-        h: 900
-    }
-];
-
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-};
-
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
